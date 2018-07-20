cead9d97717173e6ece3da7ecf2895a09114c5ae
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2018-07-20 07:25:23 +0000

    Deploy jtr13/EDAV to github.com/jtr13/EDAV.git:gh-pages

---
+++ _deploy.sh
@@ -0,0 +1,28 @@
+# configure your name and email if you have not done so
+git config --global user.email "zb2223@columbia.edu"
+git config --global user.name "Zach Bogart"
+
+# clone the repository to the book-output directory
+git clone -b gh-pages \
+  https://${GITHUB_PAT}@github.com/${TRAVIS_REPO_SLUG}.git \
+  book-output
+cd book-output
+
+git mv _book .b
+git rm -rf *
+git mv .b b
+git cp -r b/* ./
+git rm -rf b
+
+
+#git rm -rf . 
+
+#git rm !(_book)
+
+#cp -r ../_book/* ./
+git add --all *
+git commit -m "Update the book"
+git push -q origin gh-pages
+
+
+
